c32ee9ad1d2d1e9a5080d27b4ebce7beea9fce1b
kreiswelt <arwin.constantine@gmail.com>
2023-03-14 23:46:55 +0800

    handle nullable warning

--- Providers/TokenProvider.cs
+++ Providers/TokenProvider.cs
@@ -8,9 +8,10 @@ public class TokenProvider : IAccessTokenProvider
     public TokenProvider(ITokenAcquisition tokenAcquisition)
     {
         _tokenAcquisition = tokenAcquisition;
+        AllowedHostsValidator = new AllowedHostsValidator();
     }
 
-    public async Task<string> GetAuthorizationTokenAsync(Uri uri, Dictionary<string, object> additionalAuthenticationContext = default,
+    public async Task<string> GetAuthorizationTokenAsync(Uri uri, Dictionary<string, object>? additionalAuthenticationContext = default,
         CancellationToken cancellationToken = default)
     {
         var token = await _tokenAcquisition.GetAccessTokenForUserAsync(new[] { "User.Read", "Mail.Read", "Tasks.Read" });
